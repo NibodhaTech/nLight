5e3f99c0e0f743a2b1665d3b87d6e5b27b3f0cc8
Anish Antony <anish.antony@nibodha.com>
2015-01-11 06:23:06 +0530

    More changes

--- js/app/app.js
+++ js/app/app.js
@@ -2,16 +2,104 @@
 var nlightweb = angular.module('nlightweb', ['ngRoute','ngSanitize']);
 
 
-nlightweb.controller('mainCtrl', ['$scope', function($scope) {
+nlightweb.controller('mainCtrl',  function($scope,$http) {
+	$scope.activationkey = "adfdgsdfdg"
 	$scope.lightInfo = false;
   $scope.closeButton = function(){
 		$scope.lightInfo = false;
 	}
 	$scope.light1Click = function(){
+
+		var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+
+		var page = "https://nibodha-nlight-v1.p.mashape.com/control/getinfo/1/dfdf";
+    	  $http.get(page,config).success(function(response) {
+    		  $scope.lightdetails = response;
+    		  $scope.status = "on";
+      	})
+
 		$scope.lightInfo = true;
 	}
 	
 	$scope.roundClickLeft = function(){
-		$scope.lightInfo = true;
+		 var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+		
+		var page = "https://nibodha-nlight-v1.p.mashape.com/control/blink/3/1/"+$scope.activationkey;
+    	  $http.get(page,config).success(function(response) {
+    		  $scope.devices = response;
+    		  $scope.status = "on";
+      	})
+
 	}
-}]);
+
+	$scope.roundClickRight = function(){
+ var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+		var page = "https://nibodha-nlight-v1.p.mashape.com/control/blink/1/1/"+$scope.activationkey;
+    	  $http.get(page,config).success(function(response) {
+    		  $scope.devices = response;
+    		  $scope.status = "on";
+      	})
+		
+	}
+
+	$scope.roundClickTop = function(){
+
+		var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+
+
+		var page = "https://nibodha-nlight-v1.p.mashape.com/control/blink/2/1/"+$scope.activationkey;
+
+    	  $http.get(page,config).success(function(response) {
+    		  $scope.devices = response;
+    		  $scope.status = "on";
+      	})
+		
+	}
+
+	$scope.roundClickBottom = function(){
+
+		var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+
+
+		var page = "https://nibodha-nlight-v1.p.mashape.com/control/blink/4/1/"+$scope.activationkey;
+    	  $http.get(page,config).success(function(response) {
+    		  $scope.devices = response;
+    		  $scope.status = "on";
+      	})
+		
+	}
+
+	$scope.onoffcontrol = function(){
+
+		var config = {headers: {
+   'X-Mashape-Key': 'VssIjowCahmshHMZUAD30M7T993dp1OJPrZjsn4pUVbA65Exql'
+        }
+    };
+
+
+	var page = "https://nibodha-nlight-v1.p.mashape.com/control/blink/0/1/"+$scope.activationkey;
+    	  if($scope.status =="on"){
+    	  	 $http.get(page,config).success(function(response) {
+    		  $scope.devices = response;
+    		  $scope.status = "off";
+      	})
+
+    	  }
+    	 
+	}
+});
